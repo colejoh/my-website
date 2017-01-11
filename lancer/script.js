aece0b8d1b16d489ac2c0ab8481ca90fed15b40c
Cole Johnson <john1739@purdue.edu>
2017-01-11 18:30:43 -0500

    H

---
+++ lancer/script.js
@@ -0,0 +1,6 @@
+function toggleProjectState() {
+    document.getElementById("projects-pane").classList.toggle("closed");
+    document.getElementById("projects-pane").classList.toggle("open");
+    document.getElementById("show-projects").classList.toggle("fa-list");
+    document.getElementById("show-projects").classList.toggle("fa-times");
+}

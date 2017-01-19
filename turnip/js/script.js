81c5ab5cdc929139808ba1ff18f2d1f9e1279b0d
Cole Johnson <john1739@purdue.edu>
2017-01-18 22:05:14 -0500

    Addeded turnot

---
+++ turnip/js/script.js
@@ -0,0 +1,8 @@
+function toggleDisplayState() {
+    // document.getElementById("detail-content").classList.toggle("detail-content-hide");
+    // document.getElementById("detail-content").classList.toggle("detail-content-show");
+    document.getElementById("feed").classList.toggle("std-feed");
+    document.getElementById("feed").classList.toggle("detail-feed");
+    document.getElementById("detail").classList.toggle("hide-detail");
+    document.getElementById("detail").classList.toggle("show-detail");
+}

c9702f7bc014e84276c8bbdf52b7aa093a6a8b92
Cole Johnson <john1739@purdue.edu>
2017-03-29 20:40:31 -0400

    Removed some old stuff

--- turnip/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggleDisplayState() {
-    document.getElementById("cover").classList.toggle("open");
-    document.getElementById("feed").classList.toggle("std-feed");
-    document.getElementById("feed").classList.toggle("detail-feed");
-    document.getElementById("detail").classList.toggle("hide-detail");
-    document.getElementById("detail").classList.toggle("show-detail");
-}
--- turnup/script.js
+++
@@ -1,6 +0,0 @@
-function toggleProjectState() {
-    document.getElementById("projects-pane").classList.toggle("closed");
-    document.getElementById("projects-pane").classList.toggle("open");
-    document.getElementById("show-projects").classList.toggle("fa-list");
-    document.getElementById("show-projects").classList.toggle("fa-times");
-}
